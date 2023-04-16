b35b51c2813fb208c8c8f864810b5b3a475ffd52
xutong8 <2451836009@qq.com>
2023-04-16 17:10:55 +0800

    feat: upgrade

--- src/service/create_rel.ts
+++ src/service/create_rel.ts
@@ -6,19 +6,19 @@ import create_rel_date_fc from "./rel_date_fc";
 
 async function create_rel() {
   // 创建rel_p_rm表
-  // await create_rel_p_rm();
+  await create_rel_p_rm();
   console.log("rel_p_rm created🚗🚗🚗...");
 
   // 创建rel_p_fc表
-  // await create_rel_p_fc();
+  await create_rel_p_fc();
   console.log('rel_p_fc created🚗🚗🚗...');
 
   // 创建rel_fc_fc表
-  // await create_rel_fc_fc();
+  await create_rel_fc_fc();
   console.log('rel_fc_fc created🚗🚗🚗...');
 
   // 创建rel_date_rm表
-  // await create_rel_date_rm();
+  await create_rel_date_rm();
   console.log('rel_date_rm created🚗🚗🚗...');
 
   // 创建rel_date_fc表
--- src/service/create_tb.ts
+++ src/service/create_tb.ts
@@ -5,19 +5,19 @@ import create_tb_filter_cake from "./tb_filter_cake";
 
 async function create_tb() {
   // 创建tb_raw_material表
-  // await create_tb_raw_material();
+  await create_tb_raw_material();
   console.log("tb_raw_material created🚗🚗🚗...");
 
   // 创建tb_product_series表
-  // await create_tb_product_series();
+  await create_tb_product_series();
   console.log("tb_product_series created🚗🚗🚗...");
 
   // 创建tb_product表
-  // await create_tb_product();
+  await create_tb_product();
   console.log("tb_product created🚗🚗🚗...");
 
   // 创建tb_filter_cake表
-  // await create_tb_filter_cake();
+  await create_tb_filter_cake();
   console.log("tb_filter_cake createed🚗🚗🚗...");
   
   console.log("all tables created🌟🌟🌟...");
